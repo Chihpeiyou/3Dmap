4b6abfd7603dcb7ac86d5c6d2c21c2dd5d6949a2
Chihpeiyou <3406890983@qq.com>
2025-09-19 01:18:52 +0800

    feat: balabala

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/my-app/',   // ← 仓库名前后各一个斜杠
+  base: '/3Dmap/',   // ← 仓库名前后各一个斜杠
   build: {
     outDir: 'dist'    // 默认就是 dist，写不写都行
   }
